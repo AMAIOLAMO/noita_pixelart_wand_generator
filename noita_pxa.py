568d6ae510f4e534141e80e414ba447856bb5610
CxRedix <realcxredix@gmail.com>
2026-04-11 23:19:21 +0800

    feat: added argument parsing and removed hard coded paths;

--- noita_pxa.py
+++ noita_pxa.py
@@ -4,15 +4,15 @@ import numpy as np
 from enum import Enum
 from io import StringIO
 import ciede2000 as ciede
+import argparse
 
 
 ## This script assumes a few things:
 ## 1. You are using CE(Component Explorer)'s wiki wands to import the wands (https://noita.wiki.gg/wiki/Mod:Component_Explorer)
-## 2. you have the image file(input.png) stored in the running directory
-## 3. the preview will be stored in the preview.png for faster palette checks 
-## 4. The result will be printed out in result.txt, you need to copy and import this in CE (Component Explorer)
-## 5. you need to install pillow and numpy for python (`pip install pillow numpy`) preferably in a virtual environment :P
-## 6. finally, you simply need to run: python noita_pxa.py to process everything :) have fun
+## 2. you need to install pillow and numpy for python (`pip install pillow numpy`) preferably in a virtual environment :P
+## 3. finally, you simply need to run: python noita_pxa.py --input "input_file" --output "output_file" and you have your wand!
+## 4. you can import this through  Component Explorer's "Wiki Wands" > "import" tab paste and you are good to go
+## 5. you can optionally show a preview image by using the --preview "preview_file" path
 
 def clampf(v: float, a: float, b: float) -> float:
     return max(
@@ -37,7 +37,6 @@ class Colori:
         self.r = r
         self.g = g
         self.b = b
-        pass
 
     ## the higher the more different
     def dist_to(self, other, match_mode = ColorMatchMode.PERCEPTUAL_LINEAR) -> float:
@@ -215,7 +214,7 @@ def find_closest_color_pair(color: Colori, match_mode: ColorMatchMode):
     return best_pair
 
 
-def make_pixel_str(is_col_end: bool, color_spell_name: str) -> str:
+def make_pixel_str(is_col_end: bool, color_spell_name: str, inf_mana: bool = False) -> str:
     result = ""
     if is_col_end == False:
         result += "BURST_2,"
@@ -224,37 +223,76 @@ def make_pixel_str(is_col_end: bool, color_spell_name: str) -> str:
     # result += "NOLLA,SUPER_TELEPORT_CAST,LIFETIME_DOWN,LIFETIME_DOWN,LIFETIME_DOWN,LIFETIME," + color_spell_name + ",BLOOD_MAGIC,BLOOD_MAGIC,BLOOD_MAGIC,DEATH_CROSS_BIG,SUPER_TELEPORT_CAST"
 
     ## FIREBOMB
-    result += "NOLLA,SUPER_TELEPORT_CAST,BURST_3,BLOOD_MAGIC,BLOOD_MAGIC,ZERO_DAMAGE,ZERO_DAMAGE," + color_spell_name + ",FIREBOMB,ADD_TRIGGER,CASTER_CAST,DIGGER,PURPLE_EXPLOSION_FIELD,SUPER_TELEPORT_CAST"
+    if inf_mana == False:
+        result += "BLOOD_MAGIC,BLOOD_MAGIC,"
+
+    result += "NOLLA,SUPER_TELEPORT_CAST,BURST_3,ZERO_DAMAGE,ZERO_DAMAGE," + color_spell_name + ",FIREBOMB,ADD_TRIGGER,CASTER_CAST,DIGGER,PURPLE_EXPLOSION_FIELD,SUPER_TELEPORT_CAST"
     ## too laggy
     # result += "NOLLA,SUPER_TELEPORT_CAST,BURST_3,BLOOD_MAGIC,BLOOD_MAGIC,ZERO_DAMAGE,ZERO_DAMAGE," + color_spell_name + ",FIREBOMB,TRANSMUTATION,PURPLE_EXPLOSION_FIELD,SUPER_TELEPORT_CAST"
     return result
 
-def begin_column_str() -> str:
+def begin_column_str(inf_mana: bool = False) -> str:
+    ## DEATHCROSS
     # return "BURST_2,BLOOD_MAGIC,BLOOD_MAGIC,NOLLA,SUPER_TELEPORT_CAST,ADD_DEATH_TRIGGER,NOLLA,GRAVITY,DEATH_CROSS"
-    ## FIREBOMB
-    return "BURST_2,BLOOD_MAGIC,BLOOD_MAGIC,NOLLA,SUPER_TELEPORT_CAST,ADD_DEATH_TRIGGER,NOLLA,GRAVITY,DIGGER"
 
-print("Reading image")
-img = Image.open("input.png")
-pixels = img.load()
-width, height = img.size
+    ## FIREBOMB
+    result = ""
+
+    if inf_mana == False:
+        result += "BLOOD_MAGIC,BLOOD_MAGIC,"
+
+    result += "BURST_2,NOLLA,SUPER_TELEPORT_CAST,ADD_DEATH_TRIGGER,NOLLA,GRAVITY,DIGGER"
+
+    return result 
+
+arg_parser = argparse.ArgumentParser("Noita pixel art wand generator by CxRedix")
+arg_parser.add_argument(
+    '--input', type=str,
+    help="The input png file to start converting from, preferably lower than 128x128"
+)
+
+arg_parser.add_argument(
+    '--output', type=str,
+    help="The output text file to write the Wiki wand import to"
+)
+
+arg_parser.add_argument(
+    '--preview', type=str, nargs="?",
+    help="Optional preview png file to preview the final colors"
+)
+
+args = arg_parser.parse_args()
+
+if args.input == None:
+    arg_parser.error("--input must be specified.")
+
+if args.output == None:
+    arg_parser.error("--output must be specified.")
+
+
+
+
+print("Reading input from: " + args.input)
+input_img = Image.open(args.input)
+pixels = input_img.load()
+width, height = input_img.size
 
 
 spell_str_io = StringIO()
 spell_str_io.write("ACCELERATING_SHOT,LINE_ARC,LONG_DISTANCE_CAST\n")
-# spells_str = "ACCELERATING_SHOT,LONG_DISTANCE_CAST\n"
 
 spell_count = 0
 
 preview_pixels = [
 ]
 
+is_inf_mana = True
+
 print("Constructing Spells and colors...")
 for x in range(width):
     spell_str_io.write(",")
-    spell_str_io.write(begin_column_str())
+    spell_str_io.write(begin_column_str(is_inf_mana))
     spell_str_io.write("\n")
-    # spells_str += "," + begin_column_str() + "\n"
 
     col_pixels = []
 
@@ -268,8 +306,7 @@ for x in range(width):
         match_color_spell_name = match_color_pair[1]
 
         spell_str_io.write(",")
-        spell_str_io.write(make_pixel_str(is_end, match_color_spell_name))
-        # spells_str += "," + make_pixel_str(is_end, match_color_spell_name)
+        spell_str_io.write(make_pixel_str(is_end, match_color_spell_name, is_inf_mana))
 
         col_pixels.append((
             match_color_pair[0].r,
@@ -281,15 +318,17 @@ for x in range(width):
     preview_pixels.append(col_pixels)
     
     spell_str_io.write("\n")
-    # spells_str += "\n"
+
 print("Render Complete")
 
-print("Creating preview...")
-preview_pixels_array = np.array(preview_pixels, dtype=np.uint8)
-preview_img = Image.fromarray(preview_pixels_array)
-preview_img = preview_img.transpose(Image.TRANSPOSE)
-preview_img.save('preview.png')
-print("Preview complete...")
+if args.preview != None:
+    print("Creating preview...")
+    preview_pixels_array = np.array(preview_pixels, dtype=np.uint8)
+    preview_img = Image.fromarray(preview_pixels_array)
+    preview_img = preview_img.transpose(Image.TRANSPOSE)
+    print("Saving preview to file: " + args.preview)
+    preview_img.save(args.preview)
+    print("Preview complete...")
 
 print("Creating ce import string...")
 ce_import_str = ""
@@ -313,8 +352,8 @@ ce_import_str += ("""
 
 print("Finished ce import string...")
 
-print("Writing to result.txt")
-with open("result.txt", "w") as result_file:
+print("Writing to " + args.output)
+with open(args.output, "w") as result_file:
    result_file.write(ce_import_str)
 
 
